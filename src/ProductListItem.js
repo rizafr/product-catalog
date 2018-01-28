7aafb737e81ac6702493b516b8b8c1b89db63a09
rizafr <riza.fauzi.rahman@gmail.com>
2018-01-28 23:27:47 +0700

    [PRODUCT-CATALOG] Fix styling

--- src/ProductListItem.js
+++ src/ProductListItem.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const ProductListItem = props => {
-  return (
-    <div className="card card-product">
-      <div className="card-product-img-wrapper">
-        <img src={props.image} alt={props.name} />
-      </div>
+    return (
+        <div className="card card-product">
+            <div className="card-product-img-wrapper">
+                <img src={props.image} alt={props.name}/>
+            </div>
 
-      <div className="card-section">
-        <h3 className="card-product-name">{props.name}</h3>
-        <h5 className="card-product-price">Rp {props.price}</h5>
-        <h5 className="card-product-size">{props.size}</h5>
-      </div>
+            <div className="card-section">
+                <div className="col-md-6">
+                    <h3 className="card-product-name">{props.name}</h3>
+                    <h5 className="card-product-price">Rp {props.price}</h5>
+                </div>
+                <div className="col-md-6">
+                    <h6 className="card-product-size">
+                        {props.size}
+                    </h6>
+                </div>
+            </div>
 
-      <Route path={'/product/' + props.id} component={ProductListItem} />
-    </div>
-  );
+            <Route path={'/product/' + props.id} component={ProductListItem}/>
+        </div>
+    );
 };
 
 ProductListItem.propTypes = {
-  id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  image: PropTypes.string.isRequired
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired
 };
 
 export default ProductListItem;
